74c05bc2ddef063c719f930dd6fd111c01f4d0e5
Michel <kingkong12666@gmail.com>
2018-10-18 15:46:26 +0300

    Add pagination

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,36 +2,43 @@
   <v-container>
     <v-layout row v-if="!loading">
       <v-flex xs12 sm6 offset-sm3>
-        <v-card
-          v-for="pokemon in pokemonData"
-          class="elevation-10 mb-3"
-        >
-          <v-layout row>
-            <v-flex xs4>
-              <v-card-media
-                :src="pokemon.sprites.front_default"
-                height="160px"
-              ></v-card-media>
-            </v-flex>
-            <v-flex xs8>
-              <v-card-text>
-                <h2 class="text--primary">{{pokemon.name}}</h2>
-                <p
-                v-for="ability in pokemon.abilities"
-                >
-                Ability: {{ability.ability.name}}
-
-                </p>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn
-                  :to="'pokemon/' + pokemon.id"
-                >Open</v-btn>
-              </v-card-actions>
-            </v-flex>
-          </v-layout>
-        </v-card>
+        <v-data-iterator
+          :items="pokemonData"
+          :pagination.sync="pagination"
+          hide-actions>
+          <v-flex slot="item" slot-scope="props">
+            <v-card
+              class="elevation-10 mb-3"
+            >
+              <v-layout row>
+                <v-flex xs4>
+                  <v-card-media
+                    :src="props.item.sprites.front_default"
+                    height="160px"
+                  ></v-card-media>
+                </v-flex>
+                <v-flex xs8>
+                  <v-card-text>
+                    <h2 class="text--primary">{{props.item.name }}</h2>
+                    <p
+                    >
+                    </p>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                      :to="'pokemon/' + props.item.id"
+                    >Open</v-btn>
+                  </v-card-actions>
+                </v-flex>
+              </v-layout>
+            </v-card>
+          </v-flex>
+        </v-data-iterator>
+        <v-pagination
+          v-model="pagination.page"
+          :length="totalPages"
+        ></v-pagination>
       </v-flex>
     </v-layout>
 
@@ -44,6 +51,7 @@
         ></v-progress-circular>
       </v-flex>
     </v-layout>
+
   </v-container>
 </template>
 
@@ -56,29 +64,34 @@
   export default {
     data () {
       return {
-        pokemonCount: null,
-        pokemon: {
-          name: null,
-          image: null
+        pagination: {
+          page: 1,
+          rowsPerPage: 3
         },
-        pokemonData: {},
+        pokemonCount: null,
+        pokemonData: [],
         promises: [],
         loading: true
       }
     },
     methods: {
     },
+    computed: {
+      totalPages() {
+        return (233)
+      }
+    },
     mounted () {
       axios.get('https://pokeapi.co/api/v2/pokemon/')
         .then(response => (this.pokemonCount = response.data.count))
         .then(() => {
-          for (var id = 1; id <= (this.pokemonCount - 940); id++) {
+          for (var id = 1; id <= (this.pokemonCount - 250); id++) {
             this.promises.push(axios.get('https://cors.io/?https://pokeapi.co/api/v2/pokemon-form/' + id))
-            this.promises.push(axios.get('https://cors.io/?https://pokeapi.co/api/v2/pokemon/' + id))
+            /*this.promises.push(axios.get('https://cors.io/?https://pokeapi.co/api/v2/pokemon/' + id))*/
           }
           axios.all(this.promises).then((results) => {
             results.forEach((response) => {
-              this.pokemonData[response.data.name] = response.data
+              this.pokemonData.push(response.data)
             })
           })
             .finally(() => (this.loading = false))
